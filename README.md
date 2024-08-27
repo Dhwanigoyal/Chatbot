80df5dae4c2a95b66f05d67ae7340d0451bfc516
Dhwanigoyal <139227134+Dhwanigoyal@users.noreply.github.com>
2024-08-27 22:45:46 +0530

    uploaded

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Chatbot
+_This is a `Next.js` project which includes the latest `Llama 3.1 405B Instruct Model` using RAG Features, including `Together API`. The Frontend is brought to you by `TailwindCSS`, `MaterialUI`, `DaisyUI`. This app has also used `AiderAI` for configuration purposes._
+
+## Chatter AI - Brought to you by BetaHub
+
+`Under Development
---
+++ app/layout.js
@@ -0,0 +1,18 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Rick Astley Bot",
+  description: "Rick Astley Recommends Movies using LLM lol",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
